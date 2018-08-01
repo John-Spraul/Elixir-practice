ea73b37a11fd2062c69281774d61977defa9639e
Smithface <kylewibblesmith@gmail.com>
2018-08-01 18:48:45 -0400

    added routes to router.ex
    also added missing lines to guardian serializer

--- api/lib/app/guardian_serializer.ex
+++ api/lib/app/guardian_serializer.ex
@@ -6,4 +6,7 @@ defmodule App.GuardianSerializer do
 
   def for_token(user = %User{}), do: {:ok, "User:#{user.id}"}
   def for_token(_), do: {:error, "Uknown resource type"}
+
+  def from_token("User:" <> id), do: {:ok, Repo.get(User, String.to_integer(id))}
+  def from_token(_), do: {:error, "Unknown resource type"}
 end
--- api/web/router.ex
+++ api/web/router.ex
@@ -1,11 +1,32 @@
 defmodule App.Router do
   use App.Web, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgert
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.PLug.LoadResource
+  end
+
+  scope "/", App do
+    pipe_through :browser
+
+    get "/" Pagecontroller, :index
   end
 
   scope "/api", App do
     pipe_through :api
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only: [:create]
   end
 end
