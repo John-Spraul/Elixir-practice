02b1aeb89ddd2ab844881b4e5d8d9a491629d3c3
Smithface <kylewibblesmith@gmail.com>
2018-08-07 17:43:17 -0400

    wrote user_view.ex

--- api/web/views/user_view.ex
+++ api/web/views/user_view.ex
@@ -1,18 +1,11 @@
 defmodule App.UserView do
   use App.Web, :view
 
-  def render("index.json", %{users: users}) do
-    %{data: render_many(users, App.UserView, "user.json")}
-  end
-
-  def render("show.json", %{user: user}) do
-    %{data: render_one(user, App.UserView, "user.json")}
-  end
-
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{
+      id: user.id,
       username: user.username,
       email: user.email,
-      password_hash: user.password_hash}
+    }
   end
 end
